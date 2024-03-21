ac4d423f4067708f47358824d82f37a7e662e761
Andreasgura <andre-56@hotmail.it>
2024-03-21 16:18:51 +0100

    milestone 1 -2 -bonus 1

--- js/script.js
+++ js/script.js
@@ -21,43 +21,52 @@ Buon lavoro! :muscle:
 
 */
 
-let teamMembers = [];
 
-let teamMember1 = {
-    name: 'Wayne Barnett',
-    jobPosition: 'Founder & CEO',
-    img: 'wayne-barnett-founder-ceo.jpg'
-}
-let teamMember2 = {
-    name: 'Angela Caroll',
-    jobPosition: 'Chief Editor',
-    img: 'angela-caroll-chief-editor.jpg'
-}
-let teamMember3 = {
-    name: 'Walter Gordon',
-    jobPosition: 'Office Manager',
-    img: 'walter-gordon-office-manager.jpg'
-}
-let teamMember4 = {
-    name: 'Angela Lopez',
-    jobPosition: 'Social Media Manager',
-    img: 'angela-lopez-social-media-manager.jpg'
-}
-let teamMember5 = {
-    name: 'Scott Estrada',
-    jobPosition: 'Developer',
-    img: 'scott-estrada-developer.jpg'
-}
-let teamMember6 = {
-    name: 'Barbara Ramos',
-    jobPosition: 'Graphic Designer',
-    img: 'barbara-ramos-graphic-designer.jpg'
-}
+let teamMembers = [
+    {
+        name: 'Wayne Barnett',
+        jobPosition: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        jobPosition: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        jobPosition: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        jobPosition: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        jobPosition: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        jobPosition: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
 
-teamMembers.push(teamMember1, teamMember2, teamMember3, teamMember4, teamMember5, teamMember6)
-
-
-
+console.log(teamMembers)
+let prvaEl = document.getElementById('prova')
+for (value of teamMembers){
+    console.log(value['name']);
+    let card = document.createElement('ul')
+    card.innerHTML += `
+    <li>${value['name']}</li>
+    <li>${value['jobPosition']}</li>
+    <li><img src="img/${value['img']}"></li>
+    `
+    prvaEl.appendChild(card)
+}
 // for ( let key in studente) {
 //     console.log(studente[key]);
 // }
@@ -69,4 +78,4 @@ teamMembers.push(teamMember1, teamMember2, teamMember3, teamMember4, teamMember5
 //     const element = array[index];
     
 //  }
-// let newstudent= newstudent + 1
+// // let newstudent= newstudent 
